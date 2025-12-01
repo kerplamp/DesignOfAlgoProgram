376b583723f2b2a9ae8c64dba5ef77ca3d1ab61e
failedsyntax <144387193+failedsyntax@users.noreply.github.com>
2025-11-30 22:10:12 -0600

    Create PA2.py

---
+++ PA2.py
@@ -0,0 +1,137 @@
+
+import math
+
+citiesFile = open("cities.txt")
+citiesArray = []
+for i, line in enumerate(citiesFile):
+    splitTxt = line.split()
+    citiesArray.append([int(splitTxt[0]), float(splitTxt[1]), float(splitTxt[2])])
+
+#helper func
+def euclidianDistance(coord1, coord2):
+    xSqr = coord1[0] - coord2[0]
+    xSqr = xSqr * xSqr
+
+    ySqr = coord1[1] - coord2[1]
+    ySqr = ySqr * ySqr
+
+    dist = math.sqrt(xSqr + ySqr)
+
+    return dist
+
+#helper func
+def sqr(x):
+    return x*x
+
+#re-using merge sort code from last checkpoint 
+def merge(array1, array2, index):
+    i = 0
+    j = 0
+    mergedList = []
+    while i < len(array1) and j < len(array2):
+        if array1[i][index] < array2[j][index]:
+            mergedList.append(array1[i])
+            i = i + 1
+        else:
+            mergedList.append(array2[j])
+            j = j + 1
+    if i == len(array1):
+        mergedList.extend(array2[j:])
+    else:
+        mergedList.extend(array1[i:])
+    return mergedList
+
+
+#since we are passing an array (A) of arrays/lists (many A') and want to sort by some index in each A', we need an index variable
+def mergeSort(array, index):
+    halfArrayLen = math.floor(len(array)/2)
+    if len(array) == 1:
+        return array
+    return merge(mergeSort(array[:halfArrayLen], index), mergeSort(array[halfArrayLen:], index), index)
+
+
+
+#Brute Force
+def bruteForceClosest(cityArray, i):
+    cityArray = cityArray[:i]
+    minDist = math.inf
+    closestCities = [0, 0]
+    #we just go over each city and every city after it and compare to find the minimum
+    for i in range(len(cityArray)):
+        for j in range(i + 1, len(cityArray)):
+            cityInfoLine1 = cityArray[i]
+            cityInfoLine2 = cityArray[j]
+
+            if cityInfoLine1[0] == cityInfoLine2[0]: continue
+            curDist = euclidianDistance([cityInfoLine1[1], cityInfoLine1[2]], [cityInfoLine2[1], cityInfoLine2[2]])
+            if curDist < minDist:
+                minDist = curDist
+                closestCities = [cityInfoLine1[0], cityInfoLine2[0]]
+    return [closestCities, minDist]
+
+
+#Divide and Conquer
+def efficientClosestPair(cityArray):
+    xSortedArray = mergeSort(cityArray, 1)
+    ySortedArray = mergeSort(cityArray, 2)
+    return efficientClosestPairRecurse(xSortedArray, ySortedArray)
+
+def efficientClosestPairRecurse(xSortedArray, ySortedArray):
+    #base case (since theres only 3, brute force is fine)
+    if len(xSortedArray) <= 3:
+        return bruteForceClosest(xSortedArray, 3)
+    
+
+    xFirstHalf = xSortedArray[:math.ceil(len(xSortedArray)/2)]
+    xLastHalf = xSortedArray[math.ceil(len(xSortedArray)/2):]
+
+    medianX = xFirstHalf[-1][1]
+
+    #we pass a sorted array and pick from it instead of sorting xfirst and xlast by y (n vs 2nlogn)
+    yRelevantFirstHalf = []
+    yRelevantLastHalf = []
+    for cityInfo in ySortedArray:
+        if cityInfo[1] <= medianX:
+            yRelevantFirstHalf.append(cityInfo)
+        else:
+            yRelevantLastHalf.append(cityInfo)
+
+    #recurse
+    firstClosest, firstHalfDist = efficientClosestPairRecurse(xFirstHalf, yRelevantFirstHalf)
+    lastClosest, lastHalfDist = efficientClosestPairRecurse(xLastHalf, yRelevantLastHalf)
+
+    #cant just use min() because we need to keep track of the closest cities too
+    minDist = 0
+    if firstHalfDist < lastHalfDist:
+        minDist = firstHalfDist
+        closestCities = firstClosest
+    else:
+        minDist = lastHalfDist
+        closestCities = lastClosest
+    minDistSqr = minDist*minDist
+
+    #add all the points in the y sorted array that are closer to the median than our two current closest points
+    # (we have to check other side of median incase theres a closer point, so we use these)
+    stripArray = []
+    for cityInfo in ySortedArray:
+        if abs(cityInfo[1] - medianX) < minDist:
+            stripArray.append(cityInfo)
+
+    #i dont really fully understand the underlying logic, but some geometric law says the while loop will only run 7 times, so we have (n * O(1)) 
+    for i in range(len(stripArray) - 1):
+        k = i + 1
+        while k <= len(stripArray)-1 and sqr(stripArray[k][2] - stripArray[i][2]) < minDistSqr:
+            yDistSqr = sqr(stripArray[k][2] - stripArray[i][2])
+            xDistSqr = sqr(stripArray[k][1] - stripArray[i][1])
+            minDistSqr = min(xDistSqr + yDistSqr, minDistSqr)
+            closestCities = [stripArray[k][0], stripArray[i][0]]
+            k = k + 1
+    return [closestCities, math.sqrt(minDistSqr)]
+
+
+
+
+    
+
+print(bruteForceClosest(citiesArray, 100))
+print(efficientClosestPair(citiesArray))
